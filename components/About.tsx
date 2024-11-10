f90d97a7ff409ee6a54336bf862cc940813dd7f3
biologist01 <urwashaikh01@gmail.com>
2024-11-10 15:42:41 +0500

    about

--- components/About.tsx
+++ components/About.tsx
@@ -1,19 +1,18 @@
 // components/AboutContent.jsx
 import React from 'react';
-import styles from '../css/AboutContent.module.css';
 
 const AboutContent = () => {
   return (
-    <section className={styles.aboutContent}>
-      <h2>About Me</h2>
-      <p>
+    <section className="py-16 px-8 bg-black text-center font-sans animate-fadeIn">
+      <h2 className="text-4xl text-white">About Me</h2>
+      <p className='text-lg max-w-[800px] mx-auto text-white leading-relaxed mt-6' >
         Hello! I am a passionate web developer with a love for creating modern and user-friendly websites.
         I specialize in front-end development, particularly using React, Next.js, and Tailwind CSS, but I also
         have experience with back-end technologies like Node.js. My goal is to build fast, responsive,
         and beautiful web applications that provide excellent user experiences.
       </p>
 
-      <p>
+      <p className='text-lg max-w-[800px] mx-auto text-white leading-relaxed mt-6'>
         I am always excited to learn new technologies and improve my skills. Outside of coding, I enjoy traveling, 
         photography, and exploring the latest design trends. My mission is to continuously challenge myself and
         create innovative solutions for my clients and users.
